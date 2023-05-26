59f34fd698ccf9dae21b8a15563a8e092e5b21ae
zakirya592 <muhammadzakirya592@gmail.com>
2023-05-27 00:50:06 +0500

    updata

--- src/Companpage/Eagles Club/EaglesClub.jsx
+++ src/Companpage/Eagles Club/EaglesClub.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Sidebard from '../../Component/Sidebard/Sidebard';
-import {  GoogleApiWrapper,  } from "google-maps-react";
+import { GoogleApiWrapper, } from "google-maps-react";
 import { GoogleMap, LoadScript, StandaloneSearchBox, Marker } from '@react-google-maps/api';
 // import dotenv from "dotenv";
 // import env from "../../../.env"
@@ -22,7 +22,7 @@ const containerStyle = {
 const drawerWidth = 220
 
 function EaglesClub({ google }) {
-
+ 
     const [locationsapi, setlocationsapi] = useState([]);
 
     useEffect(() => {
@@ -39,7 +39,7 @@ function EaglesClub({ google }) {
                 console.log(err);
             })
     };
-   
+
 
     // Loaction section 
     const [selectedLocation, setSelectedLocation] = useState(null);
@@ -47,7 +47,7 @@ function EaglesClub({ google }) {
     const handleSearchBoxLoad = (ref) => {
         setSearchBox(ref);
     };
-  
+
     const handlePlacesChanged = () => {
         if (searchBox) {
             const places = searchBox.getPlaces();
@@ -82,7 +82,7 @@ function EaglesClub({ google }) {
             console.log('Geolocation is not supported by this browser.');
         }
     }, []);
-    
+
     const handleMapClicked = (event) => {
         const { latLng } = event;
         const latitude = latLng.lat();
@@ -100,12 +100,12 @@ function EaglesClub({ google }) {
             }
 
         });
-    }; 
+    };
 
- 
 
-  return (
-     <div className=''>
+
+    return (
+        <div className=''>
             <Box sx={{ display: 'flex' }}>
                 <Sidebard />
                 <AppBar
@@ -116,19 +116,19 @@ function EaglesClub({ google }) {
                         ml: { sm: `${drawerWidth}px` }
                     }}
                 ></AppBar>
-                    <Box
-                        className=''
-                        sx={{
-                            flexGrow: 1,
-                            my: 5,
-                            mx: 1,
-                            width: { sm: `calc(100% - ${drawerWidth}px)` }
-                        }}
-                    >
-                        <div className="container mx-3 mt-5" style={{width:"100%"}}>
-                    
+                <Box
+                    className=''
+                    sx={{
+                        flexGrow: 1,
+                        my: 5,
+                        mx: 1,
+                        width: { sm: `calc(100% - ${drawerWidth}px)` }
+                    }}
+                >
+                    <div className="container mx-3 mt-5" style={{ width: "100%" }}>
 
-                      {/* <Map google={google} initialCenter={{ lat: 43.68, lng: -79.43 }} zoom={12} containerStyle={containerStyle} >
+
+                        {/* <Map google={google} initialCenter={{ lat: 43.68, lng: -79.43 }} zoom={12} containerStyle={containerStyle} >
                           {locationsapi && locationsapi.map((item, index) => (
                    
                               <Marker
@@ -148,70 +148,71 @@ function EaglesClub({ google }) {
                               />
                           ))}
                       </Map> */}
+                        {/* <LoadScript googleMapsApiKey={process.env.REACT_APP_MAP_KEY}> */}
 
- <GoogleMap
-                      mapContainerStyle={{ height: '400px', width: '110%' ,}}
-                      center={selectedLocation ? { lat: selectedLocation.latitude, lng: selectedLocation.longitude } : currentLocation}
-                      zoom={10}
-                      onClick={handleMapClicked}
-                  >
-                      <StandaloneSearchBox onLoad={handleSearchBoxLoad} onPlacesChanged={handlePlacesChanged}>
-                          <input
-                              type="text"
-                              placeholder="Search for a location"
-                              style={{
-                                  boxSizing: 'border-box',
-                                  border: '1px solid transparent',
-                                  width: '240px',
-                                  height: '32px',
-                                  padding: '0 12px',
-                                  borderRadius: '3px',
-                                  boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)',
-                                  fontSize: '14px',
-                                  outline: 'none',
-                                  textOverflow: 'ellipses',
-                                  position: 'absolute',
-                                  left: '50%',
-                                  marginLeft: '-120px',
-                              }}
-                          />
-                      </StandaloneSearchBox>
+                        <GoogleMap
+                            mapContainerStyle={{ height: '400px', width: '110%', }}
+                            center={currentLocation}
+                            zoom={10}
+                            onClick={handleMapClicked}
+                        >
+                            <StandaloneSearchBox onLoad={handleSearchBoxLoad} onPlacesChanged={handlePlacesChanged}>
+                                <input
+                                    type="text"
+                                    placeholder="Search for a location"
+                                    style={{
+                                        boxSizing: 'border-box',
+                                        border: '1px solid transparent',
+                                        width: '240px',
+                                        height: '32px',
+                                        padding: '0 12px',
+                                        borderRadius: '3px',
+                                        boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)',
+                                        fontSize: '14px',
+                                        outline: 'none',
+                                        textOverflow: 'ellipses',
+                                        position: 'absolute',
+                                        left: '50%',
+                                        marginLeft: '-120px',
+                                    }}
+                                />
+                            </StandaloneSearchBox>
 
-                      {currentLocation && <Marker position={currentLocation} />}
+                            {currentLocation && <Marker position={currentLocation} />}
 
-                      {/* {selectedLocation && ( */}
-                  { locationsapi.map((item, index) => (
-                          <Marker
-                              position={{
-                              lat: parseFloat(item.lattitiude), // Ensure latitude is parsed as a float
-                              lng: parseFloat(item.longitude),
-                              }}
-                              address={item.address}
-                          label={{
-                              text: `${item.lattitiude.toString()} , ${item.longitude}`,
-                              color: 'orange',
-                              className: 'marker-label d-flex', // Apply a CSS class for custom styling
-                              fontSize: '16px', // Set the font size of the label
-                              display: 'flex', // Apply flex display to the label
-                          }} 
-                          >
+                            {locationsapi.map((item, index) => (
+                                <Marker
+                                    position={{
+                                        lat: parseFloat(item.lattitiude), // Ensure latitude is parsed as a float
+                                        lng: parseFloat(item.longitude),
+                                    }}
+                                    address={item.address}
+                                    label={{
+                                        text: `${item.lattitiude.toString()} , ${item.longitude}`,
+                                        color: 'orange',
+                                        className: 'marker-label d-flex', // Apply a CSS class for custom styling
+                                        fontSize: '16px', // Set the font size of the label
+                                        display: 'flex', // Apply flex display to the label
+                                    }}
+                                >
 
-                          </Marker>
-                  ))}
-                      {/* )} */}
-                  </GoogleMap>
-                      {/* <p>key {process.env.REACT_APP_MAP_KEY}</p> */}
-                        </div>
-                       
+                                </Marker>
+                            ))}
+
+                        </GoogleMap>
+
+                        {/* </LoadScript> */}
+                    </div>
+
                 </Box>
-        </Box>
+            </Box>
 
         </div >
-  )
+    )
 }
 
-// export default  GoogleApiWrapper({
-//     apiKey: "AIzaSyAUI_hqf3GJQ7c80e0rK9aki1fT6kDVuiU",
-// })(EaglesClub)
+export default  GoogleApiWrapper({
+    apiKey: `${ process.env.REACT_APP_MAP_KEY }`,
+})(EaglesClub)
 
-export default EaglesClub
+// export default EaglesClub
