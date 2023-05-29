dee0fe6e331f866c4dbfb3e5d359769d6146ecdf
zakirya592 <muhammadzakirya592@gmail.com>
2023-05-30 01:44:18 +0500

    updata

--- src/Companpage/Personalinformation.jsx
+++ src/Companpage/Personalinformation.jsx
@@ -1,4 +1,4 @@
-import React ,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Personal.css";
 import axios from 'axios'
 import { useNavigate, useParams } from "react-router-dom";
@@ -8,13 +8,13 @@ import { GoogleMap, LoadScript, StandaloneSearchBox, Marker } from '@react-googl
 import { Modal, Button, Form } from "react-bootstrap";
 // import { ChatState } from '../CreateContext';
 function Personalinformation() {
-    const navigate = useNavigate();
-    const personalna=()=>{
-      navigate("/Personalinformation");
-    }
-    const Detailnagation = () => {
-        navigate("/Sendinquiry");
-    }
+  const navigate = useNavigate();
+  const personalna = () => {
+    navigate("/Personalinformation");
+  }
+  const Detailnagation = () => {
+    navigate("/Sendinquiry");
+  }
   const [passwordShown, setPasswordShown] = useState(false)
   const [first_name, setfirst_name] = useState()
   const [last_name, setlast_name] = useState('')
@@ -26,44 +26,49 @@ function Personalinformation() {
   const [password, setpassword] = useState('')
 
   const [DropDownProvince, setDropDownProvince] = useState([]);
-  
-  const apicall = (province) => {
-        axios.get(`http://gs1ksa.org:3015/api/ListOfDropDownProvince`)
-            .then((res) => {
-              setDropDownProvince(res.data.recordset);
-              // console.log(res.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
-    useEffect(() => {
-        apicall();
-    }, []);
 
-
-
-  const handleProvinceChange = (event)=>{
-    setprovince(event.target.value)
-    const selectedProvince = event.target.value;
-    sessionStorage.setItem("province", event.target.value);
-   
-  }
-
-   const [DropDownCities, setDropDownCities] = useState([])
-  const Cityget = () => {
-    axios.get(`http://gs1ksa.org:3015/api/ListOfDropDownCities`)
+  const apicall = () => {
+    axios.get(`http://gs1ksa.org:3015/api/ListOfDropDownWithIDProvince`)
       .then((res) => {
-        setDropDownCities(res.data.recordset);
-        // console.log(res.data);
+        setDropDownProvince(res.data.recordset);
+        console.log(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   };
   useEffect(() => {
-    Cityget();
+    apicall();
   }, []);
+
+
+  const [selectedProvince, setSelectedProvince] = useState('');
+  const [ProvinceID, setProvinceID] = useState('');
+
+  const handleProvinceChange = (event) => {
+    // setprovince(event.target.value)
+    sessionStorage.setItem("province", event.target.value);
+    const ProvinceID = event.target.value;
+    setProvinceID(ProvinceID)
+    Cityget()
+    console.log(ProvinceID);
+  }
+
+  const [DropDownCities, setDropDownCities] = useState([])
+  const Cityget = () => {
+    axios.get(`http://gs1ksa.org:3015/api/ListOfDropDownWithIDCities/${ProvinceID}`)
+      .then((res) => {
+        setDropDownCities(res.data.recordset);
+        console.log("city", res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  
+
+
 
 
   // Design section 
@@ -139,33 +144,33 @@ function Personalinformation() {
       }
 
     });
-  };  
-  
+  };
+
   return (
     <>
-<div className='Backgroundimgsection'>s</div>
+      <div className='Backgroundimgsection'>s</div>
       <div className="px-3 text-start">
-              <button type="button" className=" btnnexttop px-5 mt-4 py-3" onClick={personalna}>PERSONAL INFORMATION</button>
-              <button type="button" className=" btnnexttopnacactive  ms-3 px-5 mt-4 py-3" onClick={Detailnagation}>TFOE-PE DETAILS</button>
-              
-           <div className="row  p-2 mx-auto formsection">
+        <button type="button" className=" btnnexttop px-5 mt-4 py-3" onClick={personalna}>PERSONAL INFORMATION</button>
+        <button type="button" className=" btnnexttopnacactive  ms-3 px-5 mt-4 py-3" onClick={Detailnagation}>TFOE-PE DETAILS</button>
 
-            <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
-                  <div className="mb-3">
-                          <label htmlFor="FirstName" className="form-label labeinput">Email*</label>
-              <input  className="form-control inputsection py-3" id="FirstName" placeholder='Enter your Email' aria-describedby="emailHelp" 
+        <div className="row  p-2 mx-auto formsection">
+
+          <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
+            <div className="mb-3">
+              <label htmlFor="FirstName" className="form-label labeinput">Email*</label>
+              <input className="form-control inputsection py-3" id="FirstName" placeholder='Enter your Email' aria-describedby="emailHelp"
                 types='email'
-                                value={email}
-                                onChange={e => {
-                                    setemail(e.target.value)
-                                  sessionStorage.setItem("email", e.target.value);
-                                }} />
-                  </div>
-                </div>
+                value={email}
+                onChange={e => {
+                  setemail(e.target.value)
+                  sessionStorage.setItem("email", e.target.value);
+                }} />
+            </div>
+          </div>
 
-                <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
-                  <div className="mb-3 position-relative">
-                          <label htmlFor="password" className="form-label labeinput">Password*</label>
+          <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
+            <div className="mb-3 position-relative">
+              <label htmlFor="password" className="form-label labeinput">Password*</label>
               <input className="form-control inputsection py-3" type={passwordShown ? 'text' : 'password'}
                 id='password'
                 placeholder='*********'
@@ -176,111 +181,112 @@ function Personalinformation() {
 
                 }}
                 required
-                />
-                 <p
-                                className='position-absolute text-end showpassiconss '
-                                onClick={()=>{ setPasswordShown(!passwordShown)}}
-                            >
-                                {passwordShown ? <EyeOutlined /> :<EyeInvisibleOutlined />}
-                            </p>
-                  </div>
-                </div>
+              />
+              <p
+                className='position-absolute text-end showpassiconss '
+                onClick={() => { setPasswordShown(!passwordShown) }}
+              >
+                {passwordShown ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+              </p>
+            </div>
+          </div>
 
-                <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
-                  <div className="mb-3">
-                          <label htmlFor="FirstName" className="form-label labeinput">First Name*</label>
+          <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
+            <div className="mb-3">
+              <label htmlFor="FirstName" className="form-label labeinput">First Name*</label>
               <input type="text" className="form-control inputsection py-3" id="FirstName" placeholder='Enter your First Name' aria-describedby="emailHelp" value={first_name}
                 onChange={(event) => {
                   setfirst_name(event.target.value)
                   sessionStorage.setItem("item_key", event.target.value);
                 }} />
-                  </div>
-                </div>
+            </div>
+          </div>
 
-                <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
-                  <div className="mb-3">
-                          <label htmlFor="LastName" className="form-label labeinput">Last Name*</label>
+          <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
+            <div className="mb-3">
+              <label htmlFor="LastName" className="form-label labeinput">Last Name*</label>
               <input type="text" className="form-control inputsection py-3" id="LastName" aria-describedby="emailHelp" placeholder='Enter your Last Name' value={last_name}
                 onChange={(event) => {
                   setlast_name(event.target.value)
                   sessionStorage.setItem("last_name", event.target.value);
                 }}
-                />
-                  </div>
-                </div>
+              />
+            </div>
+          </div>
 
-                <div className="col-12 mt-3">
-                      <div className="mb-3">
-                      <label htmlFor="addrass" className="form-label labeinput">Street Address*</label>
-                 <div>
+          <div className="col-12 mt-3">
+            <div className="mb-3">
+              <label htmlFor="addrass" className="form-label labeinput">Street Address*</label>
+              <div>
                 <Form.Control
                   type="text" className="form-control inputsection py-3 bg-transparent" placeholder='Your Street Address '
                   value={selectedLocation ? selectedLocation.address : ""}
-                readOnly
-                disabled
+                  readOnly
+                  disabled
                 />
                 {/* <p className="form-control inputsection py-4" id="addrass" placeholder='Enter your Street Address ' 
                 >
                   {localStorage.getItem('address')}
                   </p> */}
-                    </div> 
-                  </div>
-                </div>
-
-              <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
-                  <div className="mb-3">
-                      <label htmlFor="floatingSelectGrid" className="form-label labeinput">Barangay*</label>
-                      <select className="form-select inputsection py-3"  id="floatingSelectGrid" aria-label="Floating label select example" value={barangay}
-                                  onChange={(event) => {
-                                    setbarangay(event.target.value)
-                                    sessionStorage.setItem("barangay", event.target.value);
-                                  }}>
-                          <option selected >Enter/Select Barangay</option>
-                          <option value={"First"}>One</option>
-                          <option value={"Second"}>Two</option>
-                          <option value={"three"}>Three</option>
-                      </select>
-                      </div>
               </div>
+            </div>
+          </div>
 
-              <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
-                  <div className="mb-3">
-                      <label htmlFor="floatingSelectGrid2" className="form-label labeinput">Province*</label>
-                      <select className="form-select inputsection py-3" id="floatingSelectGrid2" aria-label="Floating label select example" value={province}
-                                  onChange={handleProvinceChange }>
-                          <option selected >Enter/Select Province</option>
-                          {
-                  DropDownProvince && DropDownProvince.map((itme,index)=>{
-                              return(
-                                <option key={itme.id}  value={itme.value}>{itme.provincename}</option>
-                              )
-                            })
-                          }
-                      </select></div>
-              </div>
+          <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
+            <div className="mb-3">
+              <label htmlFor="floatingSelectGrid" className="form-label labeinput">Barangay*</label>
+              <select className="form-select inputsection py-3" id="floatingSelectGrid" aria-label="Floating label select example" value={barangay}
+                onChange={(event) => {
+                  setbarangay(event.target.value)
+                  sessionStorage.setItem("barangay", event.target.value);
+                }}>
+                <option selected >Enter/Select Barangay</option>
+                <option value={"First"}>One</option>
+                <option value={"Second"}>Two</option>
+                <option value={"three"}>Three</option>
+              </select>
+            </div>
+          </div>
 
-              <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
-                  <div className="mb-3">
-                      <label htmlFor="floatingSelectGridcity" className="form-label labeinput">City*</label>
-                      <select className="form-select inputsection py-3" id="floatingSelectGridcity" aria-label="Floating label select example" value={city}
-                                  onChange={(event) => {
-                                    setcity(event.target.value)
-                                    // setSelectedCity(event.target.value)
-                                    sessionStorage.setItem("city", event.target.value);
-                                  }}>
-                          <option selected >Enter/Select City</option>
-                           {
-                  DropDownCities && DropDownCities.map((itme,index)=>{
-                              return(
-                                <option key={itme.id} value={itme.value}>{itme.Citiyname}</option>
-                              )
-                            })
-                          }
-                      </select></div>
-              </div>
+          <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
+            <div className="mb-3">
+              <label htmlFor="floatingSelectGrid2" className="form-label labeinput">Province*</label>
+              <select className="form-select inputsection py-3" id="floatingSelectGrid2" aria-label="Floating label select example"
+                value={ProvinceID}
+                onChange={handleProvinceChange}>
+                <option selected >Enter/Select Province</option>
+                {
+                  DropDownProvince && DropDownProvince.map((itme, index) => {
+                    return (
+                      <option key={itme.id} value={itme.provinceID}>{itme.provincename}{localStorage.setItem('provinceID', itme.id)}</option>
+                    )
+                  })
+                }
+              </select></div>
+          </div>
 
-              <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
-                  <div className="mb-3">
+          <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
+            <div className="mb-3">
+              <label htmlFor="floatingSelectGridcity" className="form-label labeinput">City*</label>
+              <select className="form-select inputsection py-3" id="floatingSelectGridcity" aria-label="Floating label select example" value={city}
+                onChange={(event) => {
+                  setcity(event.target.value)
+                  // setSelectedCity(event.target.value)
+                  sessionStorage.setItem("city", event.target.value);
+                }}>
+                <option selected >Enter/Select City</option>
+                {
+                  DropDownCities && DropDownCities.map((itme, index) => {
+                    return (
+                      <option key={itme.id} value={itme.value}>{itme.Citiyname}</option>
+                    )
+                  })
+                }
+              </select></div>
+          </div>
+
+          <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
+            <div className="mb-3">
               <p htmlFor="floatingSelectGridcity" className="form-label labeinput">My Location*</p>
               <button className='fs-6 py-3 w-100 loactiontak px-2 fw-bold bg-light border border-secondary loactioncolor' onClick={handleShowModal}>
                 Pick your Location
@@ -296,12 +302,12 @@ function Personalinformation() {
                   <div className="fs-6 text-danger px-2">{error ? `Error: ${error}` : 'Fetching geolocation...'}</div>
                 )}
               </div>
-                      </div>
-              </div>
-              </div>
-        <button type="button" className="btn btnnext my-3 py-3 px-5" onClick={Detailnagation}>Next</button>
+            </div>
           </div>
-          
+        </div>
+        <button type="button" className="btn btnnext my-3 py-3 px-5" onClick={Detailnagation}>Next</button>
+      </div>
+
 
       <Modal show={showModal} onHide={handleCloseModal} size="lg">
         <Modal.Header closeButton>
