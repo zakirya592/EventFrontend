55914ccdd8a29ea25bbb4281fdbb01e0127a85b4
zakirya592 <muhammadzakirya592@gmail.com>
2023-06-01 17:03:06 +0500

    updata

--- src/Companpage/Loaction.jsx
+++ src/Companpage/Loaction.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, StandaloneSearchBox, Marker } from '@react-google-maps/api';
+import axios from "axios";
 
 
-const mapOptions = {
-    zoom: 10,
-    center: { lat: 10, lng: 10 }, // Set initial center to (0, 0)
-};
+
 function Loaction() {
     
   
@@ -27,14 +25,54 @@ function Loaction() {
         }
     }, []);
 
+    const mapOptions = {
+        zoom: 10,
+        center: { currentLocation }, // Set initial center to (0, 0)
+    };
+
+    const [locationsapi, setlocationsapi] = useState([]);
+
+  
+
+    const fetchLocations = () => {
+        axios.get(`http://gs1ksa.org:3015/api/ListOFAllLocation`)
+            .then((res) => {
+                setlocationsapi(res.data.recordset);
+                console.log(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    };
+    useEffect(() => {
+        fetchLocations();
+    }, []);
   return (
     <div>
          
        
           <div className="App">
               
-              <GoogleMap mapContainerStyle={{ width: '100%', height: '400px' }} options={mapOptions}>
+              <GoogleMap mapContainerStyle={{ width: '100%', height: '400px' }} options={mapOptions} center={currentLocation}>
                   {currentLocation && <Marker position={currentLocation} />}
+                  {locationsapi.map((item, index) => (
+                      <Marker
+                          position={{
+                              lat: parseFloat(item.lattitiude), // Ensure latitude is parsed as a float
+                              lng: parseFloat(item.longitude),
+                          }}
+                          address={item.address}
+                          label={{
+                              text: `${item.lattitiude.toString()} , ${item.longitude}`,
+                              color: 'orange',
+                              className: 'marker-label d-flex', // Apply a CSS class for custom styling
+                              fontSize: '16px', // Set the font size of the label
+                              display: 'flex', // Apply flex display to the label
+                          }}
+                      >
+
+                      </Marker>
+                  ))}
               </GoogleMap>
           </div>
 
